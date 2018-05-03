f1d88278d4990c66c8d6ba87677e786ef84533d2
muclas <mucsi.laszlo77@gmail.com>
2018-05-03 21:56:57 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# PausableTimer
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Timer from './components/timer';
+
+export default function App() {
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
+};
+
